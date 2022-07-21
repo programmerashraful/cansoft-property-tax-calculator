ef97ec28d558b7b5e40850606c387864cafa8094
Ashraful Islam <programmerashraful@gmail.com>
2022-07-21 15:46:29 +0600

    update

--- cansoft-property-tax-calculator.php
+++ cansoft-property-tax-calculator.php
@@ -3,3 +3,25 @@
 /*
     @package cansoft-property-tax-calculator
 */
+
+/*
+    Plugin Name: Cansoft Property  Tax Calculator
+    Plugin Url: https://github.com/programmerashraful/cansoft-property-tax-calculator
+    Description:  This  is a custom plugin for property tax calculator for canada.
+    Author Name:  Ashraful Islam
+    Author URI: https://github.com/programmerashraful
+    License: Update Letter
+    Text Domain: cansoft-property
+*/
+
+
+//checking the identification of access this plugin
+if( ! define('ABSPATH') ){
+    die();
+}
+defined('ABSPATH') or die('Hey you can\'t access file. you silly human!');
+
+if( ! function_exists('add_action') ){
+    echo 'Hey you can\'t access file. you silly human!';
+    exit();
+}
---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Direct Access Block</title>
+</head>
+<body>
+    <h1>Direct access is block</h1>
+</body>
+</html>
